22361f689b90b183cf535b28db9d483e2649eeb7
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-02-07 21:58:44 -0300

    feat: add responsive design

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -23,7 +23,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/60" />
-        <Dialog.Content className="fixed overflow-hidden top-1/2 left-1/2 max-w-[640px] w-full h-[60vh] -translate-x-1/2 -translate-y-1/2 rounded-md bg-slate-700 flex flex-col outline-none">
+        <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:top-1/2 md:left-1/2 md:max-w-[640px] w-full md:h-[60vh] md:-translate-x-1/2 md:-translate-y-1/2 md:rounded-md bg-slate-700 flex flex-col outline-none">
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
